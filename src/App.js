982b6c84071cb3e56ade2e826e0fa0fd4bfe2290
Okoro Ikechukwu <ikeshegs@gmail.com>
2020-08-12 17:36:17 +0100

    publish on gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import SVG from './404.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="app__container">
+      <div className="app__image">
+        <img src={SVG} alt="A boy lost in space" height="70%" width="70%" />
+      </div>
+      <div className="app__text">
+        <h1 className="app__text--big">An error occurred</h1>
+        <p className="app__text--small">
+          Seems like you got lost.
+          <span className="app__text--link"> <a href="#">Reload</a> </span>
+          this page to try again.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+import { shallow, mount } from "enzyme";
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+
+  let container;
+  beforeEach(() => container = mount(<App />))
+  it('should render 3 divs', () => {
+    expect(container.find('div').length).toEqual(3);
+  })
+
+  it('should render an image', () => {
+    expect(container.find('img').length).toEqual(1);
+  })
+
+  it('should render an h1', () => {
+    expect(container.find('h1').length).toEqual(1);
+  })
+
+  it('should render a p tag', () => {
+    expect(container.find('p').length).toEqual(1);
+  })
+
+  it('should render an anchor tag', () => {
+    expect(container.find('a').length).toEqual(1);
+  })
+
+  it('should render a span tag', () => {
+    expect(container.find('span').length).toEqual(1);
+  })
+})
