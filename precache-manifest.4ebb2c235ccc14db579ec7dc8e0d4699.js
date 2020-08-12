8b70af2b4fac8759aa74d02b0299d039b3bdbacc
Okoro Ikechukwu <ikeshegs@gmail.com>
2020-08-12 17:34:18 +0100

    Updates

---
+++ precache-manifest.4ebb2c235ccc14db579ec7dc8e0d4699.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ddccbec02f0925758c4154557f5904",
+    "url": "/error-page-with-react/index.html"
+  },
+  {
+    "revision": "30e8583c827da43b9f2d",
+    "url": "/error-page-with-react/static/css/main.f8941c9f.chunk.css"
+  },
+  {
+    "revision": "18b15660b5e1a1c59655",
+    "url": "/error-page-with-react/static/js/2.82316a22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/error-page-with-react/static/js/2.82316a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e8583c827da43b9f2d",
+    "url": "/error-page-with-react/static/js/main.8437c25d.chunk.js"
+  },
+  {
+    "revision": "22508d7721d00efff9eb",
+    "url": "/error-page-with-react/static/js/runtime-main.6553cbd9.js"
+  },
+  {
+    "revision": "b9e9b86c11dc59076e3b0f8aede37676",
+    "url": "/error-page-with-react/static/media/404.b9e9b86c.svg"
+  }
+]);
