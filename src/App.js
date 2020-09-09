64d80a48e85d24403e3d0873da45324f55be7568
Okoro Ikechukwu <ikeshegs@gmail.com>
2020-09-09 15:01:30 +0100

    subscription-ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import SVG from './404.svg';
-import './App.css';
+// import SVG from './404.svg';
+
+import SubcriptionUI from './sub'
+// import './App.css';
 
 function App() {
   return (
-    <div className="app__container">
-      <div className="app__image">
-        <img src={SVG} alt="A boy lost in space" height="70%" width="70%" />
-      </div>
-      <div className="app__text">
-        <h1 className="app__text--big">An error occurred</h1>
-        <p className="app__text--small">
-          Seems like you got lost.
-          <span className="app__text--link"> <a href="#">Reload</a> </span>
-          this page to try again.
-        </p>
-      </div>
-    </div>
+    // <div className="app__container">
+    //   <div className="app__image">
+    //     <img src={SVG} alt="A boy lost in space" height="70%" width="70%" />
+    //   </div>
+    //   <div className="app__text">
+    //     <h1 className="app__text--big">An error occurred</h1>
+    //     <p className="app__text--small">
+    //       Seems like you got lost.
+    //       <span className="app__text--link"> <a href="#">Reload</a> </span>
+    //       this page to try again.
+    //     </p>
+    //   </div>
+    // </div>
+    <SubcriptionUI />
   );
 }
 
